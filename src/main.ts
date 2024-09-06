8cf0047420c705f2a1fb9c7d96c036c8ffe01171
Erick <esantostaype@gmail.com>
2024-09-06 00:50:58 -0500

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import session from 'express-session';
-import passport from 'passport'
+import * as session from 'express-session';
+import * as passport from 'passport'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
